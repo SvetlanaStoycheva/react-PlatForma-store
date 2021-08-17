ad1d835578a5e150188db8aa3038a98e9138343b
Svetlana Stoycheva <s.stoycheva@gmail.com>
2021-08-17 19:08:39 +0300

    product page with all products and sort completed

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -7,11 +7,52 @@ import {
   UPDATE_FILTERS,
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
-} from '../actions'
+} from '../actions';
 
 const filter_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  if (action.type === LOAD_PRODUCTS) {
+    //it needs to be with spread operator... bc otherwise both all_ and filtered_ point to the same place in the memory in JS. And when we changed the one we will be changing the other too. So we are coping the values, we are not referencing to the same place in the memory.
+    return {
+      ...state,
+      all_products: [...action.payload],
+      filtered_products: [...action.payload],
+    };
+  }
+  if (action.type === SET_GRIDVIEW) {
+    return { ...state, grid_view: true };
+  }
+  if (action.type === SET_LISTVIEW) {
+    return { ...state, grid_view: false };
+  }
+  if (action.type === UPDATE_SORT) {
+    return { ...state, sort: action.payload };
+  }
+  if (action.type === SORT_PRODUCTS) {
+    const { sort, filtered_products } = state;
+    let newProducts = [...filtered_products];
+    if (sort === 'choose-criteria') {
+      newProducts = [...state.all_products];
+    }
+    if (sort === 'price-lowest') {
+      newProducts = filtered_products.sort((a, b) => a.price - b.price);
+    }
+    if (sort === 'price-highest') {
+      newProducts = filtered_products.sort((a, b) => b.price - a.price);
+    }
+    if (sort === 'name-a') {
+      newProducts = filtered_products.sort((a, b) =>
+        a.name.localeCompare(b.name)
+      );
+    }
+    if (sort === 'name-z') {
+      newProducts = filtered_products.sort((a, b) =>
+        b.name.localeCompare(a.name)
+      );
+    }
+    return { ...state, filtered_products: newProducts };
+  }
+  // return state;
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
 
-export default filter_reducer
+export default filter_reducer;
